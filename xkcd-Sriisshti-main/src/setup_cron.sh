871789579a5f33e020876d945959521fe5e5b7bf
Sriisshti <138436340+Sriisshti@users.noreply.github.com>
2025-07-31 14:47:01 +0530

    Update setup_cron.sh

--- xkcd-Sriisshti-main/src/setup_cron.sh
+++ xkcd-Sriisshti-main/src/setup_cron.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
-# This script should set up a CRON job to run cron.php every 24 hours.
-# You need to implement the CRON setup logic here.
+PHP_PATH=$(which php)
+CRON_FILE="src/cron.php"
+CRON_LOG="/tmp/xkcd_cron.log"
+CRON_JOB="0 0 * * * $PHP_PATH $(pwd)/$CRON_FILE >> $CRON_LOG 2>&1"
+
+(crontab -l 2>/dev/null | grep -v "$CRON_FILE"; echo "$CRON_JOB") | crontab -
+
+echo "CRON job set up to run every 24 hours."
